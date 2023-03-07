1fbd2f0bc8938c1a05ea1cdb1ce4b5643377cfcc
C'est une pomme <dmgordienko@gmail.com>
2023-03-07 18:18:15 +0400

    Initial Commit

---
+++ FireShape/FireShapeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FireShapeApp.swift
+//  FireShape
+//
+//  Created by Дмитрий Гордиенко on 07.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct FireShapeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
